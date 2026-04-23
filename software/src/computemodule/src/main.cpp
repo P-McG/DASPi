badc40f5cadd61ceea216e1f24768f18d28f9b68
Paul McGrath <paul.s.mcgrath@gmail.com>
2026-04-23 13:52:17 -0400

    fixing face indexing

--- software/src/computemodule/src/main.cpp
+++ software/src/computemodule/src/main.cpp
@@ -1693,6 +1693,11 @@ void updateCameraImages(std::vector<CameraView>& cameras,
         if (haveFrame) {
             cam.image = latest;
         }
+        else {
+		    std::cout << "[WARN] Missing frame for cam=" << i
+		              << " module=" << cam.moduleIndex
+		              << " stream=" << cfg.localStreamIndex << '\n';
+		}
 
         std::cout << "[updateCameraImages] cam=" << i
                   << " module=" << cam.moduleIndex
